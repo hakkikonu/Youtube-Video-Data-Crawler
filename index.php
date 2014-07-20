a947d085e1f4a44091d25c1b48ebbfe555bc9556
Hakkı Konu <hakkikonu@gmail.com>
2014-07-20 04:11:05 +0300

    class test
    this is a sample file to test Youtube class

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <title>title</title>
+        <meta charset="utf-8"/>
+    </head>
+    <body>
+        <?php
+        require_once 'YoutubeVideoDataCrawler.class.php';
+        $video_id = $_GET["v"];
+
+        if (isset($video_id)) {
+          //create a new object
+          //give a youtube video id to constructor as parameter
+          $youtube = new YoutubeVideoDataCrawler($video_id);
+            
+          //and enjoy
+          echo "Title:" . $youtube->title() . "<br>";
+          echo "views:" . $youtube->views() . "<br>";
+          echo "Publish Date:" . $youtube->publishDate() . "<br>";
+          echo "Category:" . $youtube->category() . "<br>";
+          echo "Author:" . $youtube->author() . "<br>";
+          echo "Duration:" . $youtube->duration() . "<br>";
+          echo "Description:" . $youtube->description() . "<br>";
+          echo "Likes:" . $youtube->likesCount() . " and Dislikes:" . $youtube->disLikesCount() . "<br>";
+          echo "Thumb:".$youtube->thumbnails()[0]."<br>";
+          echo "Favorite:".$youtube->favoriteCount()."<br>";
+             
+        }
+        ?>
+
+    </body>
+</html>
+
+
